9d71bded389049fcd9cf204148bee87346178343
kosuke <sonbb313@gmail.com>
2022-05-07 17:03:34 +0900

    third-commit

--- js/main.js
+++ js/main.js
@@ -4,3 +4,11 @@ const target = document.getElementById("menu");
 target.addEventListener('click', () => {
   target.classList.toggle('open');
 });
+
+const menuOpen = document.querySelector(".menu")
+const menu = document.querySelector(".nav2")
+const menuClose = document.querySelector(".menu")
+
+menuOpen.addEventListener("click", function () {
+  menu.classList.add()
+})
