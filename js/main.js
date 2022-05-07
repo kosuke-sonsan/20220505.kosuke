b9b720c08a3cb795490315b26c36e4908f28d2e8
kosuke <sonbb313@gmail.com>
2022-05-07 20:55:40 +0900

    forth-commit

--- js/main.js
+++ js/main.js
@@ -1,14 +1,19 @@
 console.log("Hello world");
 
 const target = document.getElementById("menu");
-target.addEventListener('click', () => {
+target.addEventListener('click', function() {
   target.classList.toggle('open');
 });
 
-const menuOpen = document.querySelector(".menu")
-const menu = document.querySelector(".nav2")
-const menuClose = document.querySelector(".menu")
-
+const menuOpen = document.getElementsById("menu")
 menuOpen.addEventListener("click", function () {
-  menu.classList.add()
+  menu.classList.toggle("open-menu");
 })
+console.log("menuOpen");
+
+const menuClose = document.getElementById("menu")
+menuClose.addEventListener("click", function () {
+  menu.classList.toggle("close-menu");
+})
+
+console.log("menuClose");
